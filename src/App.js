d4a67c1aebd7f7decf3e04f13c30644d695994e9
iamlokeshgowda <lokesham0804@gmail.com>
2021-09-06 14:06:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import BirthdayList from './components/BirthdayList';
+
+function App() {
+  return (
+    <div className="App">
+      <BirthdayList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BirthdayList.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import BirthdayData from './BirthdayData'
+
+/* 1. need add conditional rendering with current date
+   2. add functionality to Add new bithdays */
+function BirthdayList() {
+    const [people, setPeople] = useState(BirthdayData)
+    
+    return (
+        <div className="Main-Content">
+            <p>{people.length} Birthdays today</p>
+            {people.map((person)=>{
+                const {id, name, age, image} =person
+                return(
+                    <div key={id} className=" person">
+                        <img src={image} />
+                        <div className="details">
+                            <h4>{name}</h4>
+                            <p>{age} years</p>
+                        </div>
+                    </div>
+                )
+            })}
+            <button className="btn add-btn">Add New</button>
+            <button className="btn clear-btn" onClick={()=>setPeople([])}>Clear All</button>
+        </div>
+        
+        /*<div className="container-fluid">
+            <div className="row">
+                <div className="col-md-4 mx-auto">
+                    
+                    <button className="btn btn-success px-4 mt-5">loki</button>
+                </div>
+            </div>
+        </div> */
+    )
+}
+
+export default BirthdayList
